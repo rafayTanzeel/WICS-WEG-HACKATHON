34f7bdb54be5189fac3fad27f2654f6aeb7445a5
Rafay Tanzeel <rafaytanzeel@gmail.com>
2015-11-14 11:09:00 -0800

    Added TextToVoice code

--- VisualAssistance/app/src/main/java/com/example/rafay/visualassistance/MainActivity.java
+++ VisualAssistance/app/src/main/java/com/example/rafay/visualassistance/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.rafay.visualassistance;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextToSpeech ttsobj;
+    private int result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ttsobj=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status==TextToSpeech.SUCCESS){
+                    result=ttsobj.setLanguage(Locale.UK);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Feature Not Supported", Toast.LENGTH_SHORT).show();
+
+
+                }
+
+
+
+
+            }
+        }
+        );
+
+        Button b=(Button) findViewById(R.id.voiceOutputer);
+        b.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA) {
+
+                            Toast.makeText(getApplicationContext(), "Feature Not Supported", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            ttsobj.speak("Hello", TextToSpeech.QUEUE_FLUSH, null);
+
+                        }
+                    }
+                }
+        );
+
+
+
+
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ttsobj.stop();
+        ttsobj.shutdown();
+    }
+
 }
