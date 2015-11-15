c39e81b1c225794e0b9050f9d3ef04fa4bb48fd9
Kevin Wong <kevinwong_895@hotmail.com>
2015-11-14 18:59:30 -0800

    Audio files

--- VisualAssistance/app/src/main/java/com/example/rafay/visualassistance/MainActivity.java
+++ VisualAssistance/app/src/main/java/com/example/rafay/visualassistance/MainActivity.java
@@ -1,21 +1,11 @@
 package com.example.rafay.visualassistance;
 
-import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Environment;
-import android.os.Handler;
-import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import java.io.File;
-import java.util.HashMap;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer mp;
@@ -23,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mp = MediaPlayer.create(MainActivity.this, R.raw.speech);
+        mp = MediaPlayer.create(MainActivity.this, R.raw.welcome);
         mp.start();
 
         Button b=(Button) findViewById(R.id.movegy);
