8e4a3831d040ec8f768cc011af0a34939f614568
Rafay Tanzeel <rafaytanzeel@gmail.com>
2015-11-14 18:55:42 -0800

    editted phone number

--- VisualAssistance/app/src/main/java/com/example/rafay/visualassistance/SecondMainActivity.java
+++ VisualAssistance/app/src/main/java/com/example/rafay/visualassistance/SecondMainActivity.java
@@ -1,9 +1,12 @@
 package com.example.rafay.visualassistance;
 
 import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -12,8 +15,10 @@ import android.provider.Contacts;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class SecondMainActivity extends AppCompatActivity {
@@ -26,12 +31,24 @@ public class SecondMainActivity extends AppCompatActivity {
         Button yes = (Button) findViewById(R.id.acceptedYes);
         Button no = (Button) findViewById(R.id.acceptedNo);
 
+        AccountManager am = AccountManager.get(this);
+        Account[] accounts = am.getAccounts();
+        StringBuilder str=new StringBuilder();
+        EditText ed=(EditText)findViewById(R.id.editTexttt);
+
+
+        ed.setText(getPhoneNumber("Mom",this));
+
+
         yes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                call();
             }
         });
+
+
+
     }
 
     private void call() {
@@ -42,6 +59,21 @@ public class SecondMainActivity extends AppCompatActivity {
         catch (android.content.ActivityNotFoundException ex){
             Toast.makeText(getApplicationContext(), "yourActivity is not founded", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public String getPhoneNumber(String name, Context context) {
+        String ret = null;
+        String selection = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" like'%" + name +"%'";
+        String[] projection = new String[] { ContactsContract.CommonDataKinds.Phone.NUMBER};
+        Cursor c = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                projection, selection, null, null);
+        if (c.moveToFirst()) {
+            ret = c.getString(0);
+        }
+        c.close();
+        if(ret==null)
+            ret = "Unsaved";
+        return ret;
     }
 
 }
