b46687bad7a1b7e1da7e39abbde6fda341803a53
Rafay Tanzeel <rafaytanzeel@gmail.com>
2015-11-14 16:50:09 -0800

    fixed text to speech

--- VisualAssistance/app/src/main/java/com/example/rafay/visualassistance/MainActivity.java
+++ VisualAssistance/app/src/main/java/com/example/rafay/visualassistance/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.rafay.visualassistance;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,67 +12,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private TextToSpeech ttsobj;
-    private int result;
+    private MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        ttsobj=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if(status==TextToSpeech.SUCCESS){
-                    result=ttsobj.setLanguage(Locale.UK);
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Feature Not Supported", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-        );
 
-        if (result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA) {
-
-            Toast.makeText(getApplicationContext(), "Feature Not Supported", Toast.LENGTH_SHORT).show();
-
-        } else {
-            ttsobj.speak("Where would you like to go", TextToSpeech.QUEUE_FLUSH, null);
-
-        }
-
-
-
+        mp = MediaPlayer.create(MainActivity.this, R.raw.speech);
+        mp.start();
 
         Button b=(Button) findViewById(R.id.voiceOutputer);
         b.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if (result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA) {
-
-                            Toast.makeText(getApplicationContext(), "Feature Not Supported", Toast.LENGTH_SHORT).show();
-
-                        } else {
-                            ttsobj.speak("Welcome to Visual Assistance, click the button in the center to state location", TextToSpeech.QUEUE_FLUSH, null);
-
-                        }
+                                mp.start();
                     }
                 }
         );
 
 
-
-
     }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        ttsobj.stop();
-        ttsobj.shutdown();
+    protected void onPause() {
+        super.onPause();
+        mp.release();
     }
 
 }
--- VisualAssistance/app/src/main/java/com/example/rafay/visualassistance/Splash_Screen.java
+++ VisualAssistance/app/src/main/java/com/example/rafay/visualassistance/Splash_Screen.java
@@ -2,88 +2,64 @@ package com.example.rafay.visualassistance;
 
 
 import android.content.Intent;
-import android.speech.tts.TextToSpeech;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import java.util.Locale;
+
+
 
 public class Splash_Screen extends AppCompatActivity {
-    public TextToSpeech ttsobj;
-    private int result;
+
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
 
-        ttsobj=new TextToSpeech(getBaseContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if(status==TextToSpeech.SUCCESS){
-                    result=ttsobj.setLanguage(Locale.UK);
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Feature Not Supported", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-        );
-
+        mp = MediaPlayer.create(Splash_Screen.this, R.raw.speech);
         final ImageView iv=(ImageView)findViewById(R.id.logoPic);
         final Animation an= AnimationUtils.loadAnimation(getBaseContext(),R.anim.logo_rotates);
         final Animation an2= AnimationUtils.loadAnimation(getBaseContext(),R.anim.logo_fade);
 
-        iv.startAnimation(an);
+        iv.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        iv.startAnimation(an);
+                    }
+                }
+        );
+
+        iv.setOnLongClickListener(
+                new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        iv.startAnimation(an2);
+                        finish();
+                        Intent i = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(i);
+                        return false;
+                    }
+                }
+        );
+
 
         an.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                if (result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA) {
-                    Toast.makeText(getApplicationContext(), "Feature Not Supported", Toast.LENGTH_SHORT).show();
-
-                } else {
-                    ttsobj.stop();
-                    ttsobj.speak("Welcome to Visual Assistance, hold the button for 3 seconds in the center to state location", TextToSpeech.QUEUE_FLUSH, null);
-                    iv.startAnimation(an);
-                }
+                mp.start();
+                iv.startAnimation(an);
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                iv.setOnClickListener(
-                        new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                if (result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA) {
-                                    Toast.makeText(getApplicationContext(), "Feature Not Supported", Toast.LENGTH_SHORT).show();
 
-                                } else {
-                                    ttsobj.stop();
-                                    ttsobj.speak("Welcome to Visual Assistance, hold the button for 3 seconds in the center to state location", TextToSpeech.QUEUE_FLUSH, null);
-                                    iv.startAnimation(an);
-                                }
-                            }
-                        }
-                );
-
-                iv.setOnLongClickListener(
-                        new View.OnLongClickListener() {
-                            @Override
-                            public boolean onLongClick(View v) {
-                                iv.startAnimation(an2);
-                                finish();
-                                Intent i = new Intent(getBaseContext(), MainActivity.class);
-                                startActivity(i);
-                                return false;
-                            }
-                        }
-                );
             }
 
             @Override
@@ -91,5 +67,14 @@ public class Splash_Screen extends AppCompatActivity {
             }
         });
 
+        iv.startAnimation(an);
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mp.release();
+    }
+
 }
